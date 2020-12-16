f8ead26488620467986d1f5f0f043e42677b9cdc
liqiangsky <12508liqiang@gmail.com>
2020-12-16 12:36:07 +0800

    router

--- dianshangsystem/src/router/index.js
+++ dianshangsystem/src/router/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login'
-import Welcome from '../views/Welcome'
+import Home from '../views/Home'
+import Welcome from '../components/Welcome'
+import Users from '../components/Users'
+import Roles from '../components/Roles'
+
+//防止重复点击路由出错问题
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', name: Login, component: ()=> import('@/views/Login.vue') },
-  { path: '/welcome', name: Welcome, component: ()=> import('@/views/Welcome.vue') }
+  { path: '/login', name: Login, component: () => import('@/views/Login.vue') },
+  {
+    path: '/home', redirect: '/welcome', name: Home, component: () => import('@/views/Home.vue'),
+    children: [
+      { path: '/welcome', name: Welcome, component: () => import('@/components/Welcome.vue') },
+      { path: '/users', name: Users, component: () => import('@/components/Users.vue') },
+      { path: '/roles', name: Roles, component: () => import('@/components/Roles.vue') }
+    ]
+  },
   // {
   //   path: '/about',
   //   name: 'About',
@@ -22,5 +38,11 @@ const routes = [
 const router = new VueRouter({
   routes
 })
-
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next();
+  const token = sessionStorage.token;
+  if (!token) return next('/login');
+  next();
+})
 export default router
