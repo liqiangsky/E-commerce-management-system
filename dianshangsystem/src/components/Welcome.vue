f14e1d230ac24b64b9fcd27e2f91012337b7c49d
liqiangsky <12508liqiang@gmail.com>
2020-12-16 12:38:14 +0800

    Welcome

---
+++ dianshangsystem/src/components/Welcome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="welcome">
+    <div class="hello m-t-50">
+      <h2>Welcome !</h2>
+      <h2>欢迎使用商城后台管理系统</h2>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="less" scoped>
+.hello {
+  height: 300px;
+  font-size: 30px;
+  font-weight: bolder;
+  color: rgb(23, 211, 228);
+  display: flex;
+  text-align: center;
+  flex-flow: column;
+  justify-content: space-between;
+}
+</style>
