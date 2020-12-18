3f951204419e2bb3a3033f65036665ede3562a8e
liqiangsky <12508liqiang@gmail.com>
2020-12-18 20:46:38 +0800

    main

--- dianshangsystem/src/main.js
+++ dianshangsystem/src/main.js
@@ -5,11 +5,17 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui';
+import VueQuillEditor from 'vue-quill-editor'
 import 'element-ui/lib/theme-chalk/index.css';
 //全局样式
 import './assets/css/gloabl.css'
+//富文本编辑器
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.prototype.$http = axios;
+Vue.use(VueQuillEditor)
 Vue.use(VueAxios, axios);
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
@@ -25,6 +31,17 @@ axios.interceptors.response.use((response) => {
 axios.interceptors.request.use(config => {
   config.headers.Authorization = sessionStorage.token;
   return config;
+})
+//全局过滤器，格式化时间
+Vue.filter('FormatDate', originval => {
+  const time = new Date(originval * 1000);
+  const year = time.getFullYear();
+  const month = (time.getMonth() + 1 + '').padStart(2, '0');
+  const day = (time.getDate() + '').padStart(2, '0');
+  const hour = (time.getHours() + '').padStart(2, '0');
+  const minute = (time.getMinutes() + '').padStart(2, '0');
+  const second = (time.getSeconds() + '').padStart(2, '0');
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 })
 
 new Vue({
