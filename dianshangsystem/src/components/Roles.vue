a9558aefa955d6be0967243dc254cab5f843f77e
liqiangsky <12508liqiang@gmail.com>
2020-12-30 17:36:24 +0800

    Roles

--- dianshangsystem/src/components/Roles.vue
+++ dianshangsystem/src/components/Roles.vue
@@ -219,9 +219,6 @@ export default {
       node.children.forEach((item) => this.GetCheckList(item, arr));
     },
     CloseTag(row, right, arr) {
-      console.log(row);
-      console.log(right);
-      console.log(arr, "arr");
       this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
